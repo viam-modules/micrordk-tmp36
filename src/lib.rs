324b8b12983c640dfd77b32f44fcc6896802c3c6
Steve Briskin <steve@viam.com>
2024-06-24 11:22:49 -0400

    smoothing

--- src/lib.rs
+++ src/lib.rs
@@ -15,6 +15,7 @@ use micro_rdk::{
 use std::{
     collections::HashMap,
     sync::{Arc, Mutex},
+    thread, time,
 };
 
 pub fn register_models(registry: &mut ComponentRegistry) -> Result<(), RegistryError> {
@@ -26,6 +27,7 @@ pub fn register_models(registry: &mut ComponentRegistry) -> Result<(), RegistryE
 pub struct Tmp36Sensor {
     reader: AnalogReaderType<u16>,
     offset: f64,
+    num_readings: i32,
 }
 
 impl Tmp36Sensor {
@@ -37,10 +39,21 @@ impl Tmp36Sensor {
         let board_unwrapped = board.unwrap();
 
         let offset = _cfg.get_attribute::<f64>("offset").unwrap_or(0.0);
+        let num_readings = _cfg.get_attribute::<i32>("num_readings").unwrap_or(1);
+
+        if num_readings < 1 {
+            return Err(SensorError::ConfigError(
+                "num_readings must be an integer greater than 1",
+            ));
+        }
 
         if let Ok(analog_reader_name) = _cfg.get_attribute::<String>("analog_reader") {
             if let Ok(reader) = board_unwrapped.get_analog_reader_by_name(analog_reader_name) {
-                Ok(Arc::new(Mutex::new(Self { reader, offset })))
+                Ok(Arc::new(Mutex::new(Self {
+                    reader,
+                    offset,
+                    num_readings,
+                })))
             } else {
                 Err(SensorError::ConfigError("failed to get analog reader"))
             }
@@ -75,15 +88,38 @@ impl Readings for Tmp36Sensor {
 impl SensorT<f64> for Tmp36Sensor {
     fn get_readings(&self) -> Result<TypedReadingsResult<f64>, SensorError> {
         // TODO: deal with noise -- read X times and return the median
-        let reading = self
-            .reader
-            .lock()
-            .map_err(|_| SensorError::SensorGenericError("failed to get sensor lock"))?
-            .read()?;
-        let readingf = reading as f64;
+
+        let mut readings = Vec::new();
+        for i in 0..self.num_readings {
+            let reading = self
+                .reader
+                .lock()
+                .map_err(|_| SensorError::SensorGenericError("failed to get sensor lock"))?
+                .read()?;
+            let readingf = reading as i16;
+            readings.push(readingf);
+
+            if i < self.num_readings - 1 {
+                thread::sleep(time::Duration::from_millis(1));
+            }
+        }
+
+        // calculate median
+        readings.sort();
+        let mid = readings.len() / 2;
+        let median_reading = readings[mid];
+
         let mut x = HashMap::new();
-        x.insert("temp".to_string(), (readingf - 500.0) / 10.0 + self.offset);
-        x.insert("milliv".to_string(), readingf); // for debugging
+        x.insert(
+            "temp".to_string(),
+            (median_reading - 500) as f64 / 10.0 + self.offset,
+        ); // calculated final temp result
+
+        // debugging fields
+        x.insert("temp_raw".to_string(), (median_reading - 500) as f64 / 10.0); // temp pre-offset
+        x.insert("milliv".to_string(), median_reading as f64); // raw reading from sensor
+        x.insert("num_readings".to_string(), readings.len() as f64); // number of readings read
+
         Ok(x)
     }
 }
